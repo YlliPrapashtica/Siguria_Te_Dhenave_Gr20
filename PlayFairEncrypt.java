988ebe478f831b3ff21f362310543c0a53051bf2
IsmailiGresa <gressa.issmaili@gmail.com>
2020-03-27 21:28:55 +0100

    Update PlayFairEncrypt.java

--- PlayFairEncrypt.java
+++ PlayFairEncrypt.java
@@ -7,7 +7,7 @@ public class PlayfairCipherEncrypt
     private String Key = new String();
     private char   mat_array[][] = new char[5][5];
  
-    public void setKey(String k)
+    public void setKey(String k)  // Adds the keyword to the matrix, not allowing same letters to appear twice.
     {
         String K_fit = new String();
         boolean SIG = false;
@@ -35,10 +35,9 @@ public class PlayfairCipherEncrypt
         Key = Keyword;
         for (int i = 0; i < 26; i++)
         {
-            current = (char) (i + 97);
-            if (current == 'j')
-                continue;
-            
+            current = (char) (i + 97); // Alphabet adding 'a' ASCII.
+            if (current == 'j') 
+                continue; // Ignores letter j whenever it appears in the keyword.
             for (int j = 0; j < Keyword.length(); j++)
             {
                 if (current == Keyword.charAt(j))
@@ -48,12 +47,14 @@ public class PlayfairCipherEncrypt
                 }
             }
             if (SIG)
-                Key = Key + current;
+                Key = Key + current; // Shows how keyword applies to the alphabet without repeated letters.
             SIG = true;
         }
+        //System.out.println(Key);
+        
         matrix();
         
-    }private void matrix()
+    }private void matrix() //Shows the created matrix.
     {
         int counter = 0;
         for (int i = 0; i < 5; i++)
@@ -76,7 +77,7 @@ public class PlayfairCipherEncrypt
         len = plaintxt.length();
         for (int tmp = 0; tmp < len; tmp++)
         {
-            if (plaintxt.charAt(tmp) == 'j')
+            if (plaintxt.charAt(tmp) == 'j') //Replaces j with i.
             {
                 text = text + 'i';
             }
@@ -84,7 +85,7 @@ public class PlayfairCipherEncrypt
                 text = text + plaintxt.charAt(tmp);
         }
         len = text.length();
-        for (i = 0; i < len; i = i + 2)
+        for (i = 0; i < len; i = i + 2) //If len-1, adds an x at the end of the keyword id odd.
         {
             if (text.charAt(i + 1) == text.charAt(i))
             {
@@ -101,19 +102,19 @@ public class PlayfairCipherEncrypt
         if (size % 2 != 0)
         {
             size++;
-            Plaintext = Plaintext + 'x';
+            Plaintext = Plaintext + 'x'; //Adds an x in between when the pair is same letter.
         }
         String x[] = new String[size / 2];
         int counter = 0;
         for (int i = 0; i < size / 2; i++)
         {
-            x[i] = Plaintext.substring(counter, counter + 2);
+            x[i] = Plaintext.substring(counter, counter + 2);  //Divides characters into pairs.
             counter = counter + 2;
         }
         return x;
     }
  
-    public int[] GetDimensions(char letter)
+    public int[] GetDimensions(char letter) // Finds out the positions of characters a and b.
     {
         int[] key = new int[2];
         if (letter == 'j')
@@ -136,49 +137,51 @@ public class PlayfairCipherEncrypt
     public String encryptMessage(String Source)
     {
         String src_arr[] = DivPair(Source);
-        String Code = new String();
-        char one;
-        char two;
-        int part1[] = new int[2];
-        int part2[] = new int[2];
+        String Ciphert = new String();
+        char a;
+        char b;
+        int row[] = new int[2];
+        int col[] = new int[2];
         for (int i = 0; i < src_arr.length; i++)
         {
-            one = src_arr[i].charAt(0);
-            two = src_arr[i].charAt(1);
-            part1 = GetDimensions(one);
-            part2 = GetDimensions(two);
-            if (part1[0] == part2[0])
+            a = src_arr[i].charAt(0);
+            b = src_arr[i].charAt(1);
+            row = GetDimensions(a);
+            col = GetDimensions(b);
+            
+            if (row[0] == col[0]) // if the characters are on the same row, then select the two characters to the right of each
             {
-                if (part1[1] < 4)
-                    part1[1]++;
+                if (row[1] < 4)
+                    row[1]++;
                 else
-                    part1[1] = 0;
-                if (part2[1] < 4)
-                    part2[1]++;
+                    row[1] = 0;
+                if (col[1] < 4)
+                    col[1]++;
                 else
-                    part2[1] = 0;
+                    col[1] = 0;
             }
-            else if (part1[1] == part2[1])
+            else if (row[1] == col[1]) // else if the characters are on the same column, then select the two characters below
             {
-                if (part1[0] < 4)
-                    part1[0]++;
+                if (row[0] < 4)
+                    row[0]++;
                 else
-                    part1[0] = 0;
-                if (part2[0] < 4)
-                    part2[0]++;
+                    row[0] = 0;
+                if (col[0] < 4)
+                    col[0]++;
                 else
-                    part2[0] = 0;
+                    col[0] = 0;
             }
-            else
+            else // else the two characters are in different rows and columns, so create a square using the two points,
+    			// and select the two characters that create the two other points.
             {
-                int temp = part1[1];
-                part1[1] = part2[1];
-                part2[1] = temp;
+                int temp = row[1];
+                row[1] = col[1];
+                col[1] = temp;
             }
-            Code = Code + mat_array[part1[0]][part1[1]]
-                    + mat_array[part2[0]][part2[1]];
+            Ciphert = Ciphert + mat_array[row[0]][row[1]]
+                    + mat_array[col[0]][col[1]];
         }
-        return Code;
+        return Ciphert;
     }
  
     public static void main(String[] args)
