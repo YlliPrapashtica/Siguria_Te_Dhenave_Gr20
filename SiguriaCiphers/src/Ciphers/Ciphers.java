639f2270fbd8472e5cba2d9187c63525750b7983
YlliPrapashtica <35112499+YlliPrapashtica@users.noreply.github.com>
2020-03-29 20:24:54 +0200

    finalize

--- SiguriaCiphers/src/Ciphers/Ciphers.java
+++ SiguriaCiphers/src/Ciphers/Ciphers.java
@@ -9,16 +9,16 @@ public class Ciphers {
 	private static String Key = new String();
 	private static char mat_array[][] = new char[5][5];
 
-	public static String EncryptCeasar(String text, int key) {
+	private static String EncryptCeasar(String text, int key) {
 		StringBuffer ecnryptedText = new StringBuffer();
 
 		for (int i = 0; i < text.length(); i++) {
 
-			if (Character.isUpperCase(text.charAt(i))) {
+			if (Character.isUpperCase(text.charAt(i))) {								// if upper case  +A
 
 				char ch = (char) (((int) text.charAt(i) + key - 65) % 26 + 65);
-				ecnryptedText.append(ch);
-			} else {
+				ecnryptedText.append(ch);												
+			} else {																	// if lower case +a 
 
 				char ch = (char) (((int) text.charAt(i) + key - 97) % 26 + 97);
 				ecnryptedText.append(ch);
@@ -27,16 +27,15 @@ public class Ciphers {
 		return ecnryptedText.toString();
 	}
 
-	public static String DecryptCeasar(String ciphertext, int key) {
+	private static String DecryptCeasar(String ciphertext, int key) {
 
-		String decryptedMessage = "";
-		decryptedMessage = decryptedMessage.toUpperCase();
+		String decryptedMessage = "";				
 		char ch;
 
 		for (int i = 0; i < ciphertext.length(); ++i) {
 			ch = ciphertext.charAt(i);
 
-			if (ch >= 'a' && ch <= 'z') {
+			if (ch >= 'a' && ch <= 'z') {										//if lower case heke key
 				ch = (char) (ch - key);
 
 				if (ch < 'a') {
@@ -44,7 +43,7 @@ public class Ciphers {
 				}
 
 				decryptedMessage += ch;
-			} else if (ch >= 'A' && ch <= 'Z') {
+			} else if (ch >= 'A' && ch <= 'Z') {								// if upper case heke key
 				ch = (char) (ch - key);
 
 				if (ch < 'A') {
@@ -60,26 +59,25 @@ public class Ciphers {
 		return (decryptedMessage);
 	}
 
-	public static void BruteForceCeasar(String cipherText) {
+	private static void BruteForceCeasar(String cipherText) {
 
 		for (int i = 1; i <= 26; i++) {
 			int key = i;
-			System.out
-					.println("BruteForce Message with the key at : " + key + " is = " + DecryptCeasar(cipherText, key));
+			System.out.println("BruteForce Message with the key at : " + key + " is = " + DecryptCeasar(cipherText, key)); // provon te gjitha qelsat ne decrypt
 		}
 	}
 
 	// -------------------------------------------------------------------------------------------------------------------------------------------------------
 
 	static int[] EncryptBeale(String message, String book) {
-		int[] output = new int[message.length()];
-		char[] bookArr = book.toUpperCase().toCharArray();
+		int[] output = new int[message.length()];								// ruhet output qe del
+		char[] bookArr = book.toUpperCase().toCharArray();						// kthen book.txt ne karaktere dhe Uppercase
 
-		for (int i = 0; i < message.length(); i++) {
+		for (int i = 0; i < message.length(); i++) {							//krejt shkronjat e tekstit krahasohen dhe zevendsohen me pozitat ne liber
 			for (int j = 0; j < bookArr.length; j++) {
 
 				if (bookArr[j] == message.charAt(i)) {
-					output[i] = j;
+					output[i] = j;												// e merr vleren output-i
 					break;
 				}
 			}
@@ -88,32 +86,32 @@ public class Ciphers {
 	}
 
 	static String DecryptBeale(int[] encrypted, String book) {
-		char[] plaintextArr = new char[encrypted.length];
+		char[] plaintextArr = new char[encrypted.length];					
 		char[] bookArr = book.toCharArray();
 
 		for (int i = 0; i < encrypted.length; i++) {
 			for (int j = 0; j < bookArr.length; j++) {
-				if (j == encrypted[i] - 1) { // -1 se nuk e merr Shkronjen e pare for some reason
+				if (j == encrypted[i] - 1) { // -1 se nuk e merr Shkronjen e pare pasi qe del enkriptimi +1 ne console per bukurshkrim.
 
 					plaintextArr[i] = bookArr[j]; // replace krejt shkronjat e enkriptume
 					break;
 				}
 			}
 		}
-		return new String(plaintextArr);
+		return new String(plaintextArr);				//i bon krejt chars bashke ne nje string
 	}
 
 	static void DisplayBeale(int[] showArray) {
 		for (int i = 0; i < showArray.length; i++) {
-			System.out.print(showArray[i]+1 + " , ");
+			System.out.print(showArray[i] + 1 + " , ");				// printon ne menyre me te mire pjesen e enkriptuar e jo new line pas qdo numer
 		}
 		System.out.println("\n\n");
 	}
 
 	public static String readFileAsString(String fileName) throws Exception {
 		String file;
-		file = new String(Files.readAllBytes(Paths.get(
-				"D:\\Ylli\\Eclipse\\Siguria_Te_Dhenave_Gr20\\SiguriaCiphers\\src\\Ciphers", fileName.concat(".txt"))));
+		String currentPath = System.getProperty("user.dir").toString();    					//  pasi qe ndrron Directory zakonisht prej pc ne pc, e merr ate aktualen
+		file = new String(Files.readAllBytes(Paths.get(currentPath, fileName.concat(".txt")))); 		// nuk ka nevoje me e shenu book.txt, por vetem book
 		return file;
 	}
 
@@ -212,7 +210,7 @@ public class Ciphers {
 		return x;
 	}
 
-	public static int[] GetDimensions(char letter) // Finds out the positions of characters a and b.
+	private static int[] GetDimensions(char letter) // Finds out the positions of characters a and b.
 	{
 		int[] key = new int[2];
 		if (letter == 'j')
@@ -229,7 +227,7 @@ public class Ciphers {
 		return key;
 	}
 
-	public static String encryptPlayfair(String Source) {
+	private static String encryptPlayfair(String Source) {
 		String src_arr[] = DivPair(Source);
 		String Ciphert = "";
 		char a;
@@ -277,7 +275,7 @@ public class Ciphers {
 		return Ciphert;
 	}
 
-	public static String decryptMessage(String Code) {
+	private static String decryptMessage(String Code) {
 		String Plaint = new String();
 		String src_arr[] = DivPair(Code);
 		char a;
@@ -325,7 +323,7 @@ public class Ciphers {
 	}
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {                       // Mundet me Throw Exception te Beale kur e lyp filea
 
 		if (args[0].equalsIgnoreCase("ceasar")) {
 			if (args[1].equalsIgnoreCase("e") || (args[1].equalsIgnoreCase("encrypt"))) {
@@ -337,7 +335,6 @@ public class Ciphers {
 				System.out.println("Key is : " + key);
 				System.out.println("Cipher text is : " + EncryptCeasar(text, key).toString());
 				System.out.println("\n\n");
-				BruteForceCeasar(EncryptCeasar(text, key).toString());
 			} else if (args[1].equalsIgnoreCase("d") || (args[1].equalsIgnoreCase("decrypt"))) {
 
 				String text = args[2];
@@ -345,11 +342,12 @@ public class Ciphers {
 				System.out.println("Decrypted Message = " + DecryptCeasar(text, key));
 				System.out.println("\n");
 				BruteForceCeasar(EncryptCeasar(text, key).toString());
-				// ------------------------------------------------------------------------------------------------------------
-				// DERI KETU EKZEKUTOHET CEASAR
+			
 			} else {
 				System.out.println("Error : Wrong Command name. Try entering it again ! ");
-			}
+			}	
+			// ------------------------------------------------------------------------------------------------------------
+				// DERI KETU EKZEKUTOHET CEASAR
 
 		} else if (args[0].equalsIgnoreCase("playfair")) {
 			if (args[1].equalsIgnoreCase("e") || (args[1].equalsIgnoreCase("encrypt"))) {
@@ -359,12 +357,8 @@ public class Ciphers {
 				KeyGen();
 
 				String key_input = args[3];
-				if (key_input.length() % 2 == 0) {
-					System.out.println("Encryption: " + encryptPlayfair(key_input));
 
-				} else {
-					System.out.println("Message length is not even!");
-				}
+				System.out.println("Encryption: " + encryptPlayfair(key_input));
 
 			} else if (args[1].equalsIgnoreCase("d") || (args[1].equalsIgnoreCase("decrypt"))) {
 
@@ -372,12 +366,8 @@ public class Ciphers {
 				setKey(keyword);
 				KeyGen();
 				String key_input = args[3];
-				if (key_input.length() % 2 == 0) {
-					System.out.println("Decryption: " + decryptMessage(key_input));
 
-				} else {
-					System.out.println("Message length is not even!");
-				}
+				System.out.println("Decryption: " + decryptMessage(key_input));
 
 			} else {
 				System.out.println("Error : Wrong Command name. Try entering it again ! ");
@@ -413,13 +403,13 @@ public class Ciphers {
 				System.out.println(
 						"-----------------------------------------------------------------------------------------------------------------");
 
-				String message = args[3];
-				String[] messageSplit = message.split(" , ");
-				int[] encrypted = new int[messageSplit.length];
-				for (int i = 0; i < messageSplit.length; i++) {
-
+				String message = args[3];												//
+				String[] messageSplit = message.split(" , ");							// Pasi qe mesazhi nga cmd na vjen si String, neve na duhet te jete nje
+				int[] encrypted = new int[messageSplit.length];							//	int array per t'iu pershtatur kodit. Pra ketu vjen si String dhe
+				for (int i = 0; i < messageSplit.length; i++) {							//  ndahet ne String array me delimiter ' , ' dhe iu shtohet
+																						// int array qe na duhet.
 					encrypted[i] = Integer.parseInt(messageSplit[i]);
-					System.out.println(encrypted[i]);
+					//	Per me parse nga String ne int pra nevojitet ky kod.
 				}
 				String book = file;
 
@@ -431,8 +421,6 @@ public class Ciphers {
 			} else {
 				System.out.println("Error : Wrong Command name. Try entering it again ! ");
 			}
-			// ------------------------------------------------------------------------------------------------------------
-			// DERI KETU EKZEKUTOHET BEALE
 
 		} else {
 			System.out.println("Error : Wrong Command name. Try entering it again ! ");
@@ -440,8 +428,6 @@ public class Ciphers {
 
 	}
 
-	{
-
-	}
+	
 
 }
